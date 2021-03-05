1e430fef3a185369cfa0a94fe99c993dd06c4fba
Sony <Sony@SONY->
2021-03-05 19:54:45 +0300

    first commit Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ TestWorkEKA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestWorkEKA
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int m = 5;
+
+            int[] copiedIntArray = new int[m];
+            int[] intArray = new int[] { 1, 4, 9, 5, 3, 4, 2, 5, 3, 7 };
+
+            Array.Copy(intArray, copiedIntArray, m);
+
+            bool isExisted = Array.Exists<int>(copiedIntArray, x => x.Equals(0));
+            var convertedToInt = Convert.ToInt32(isExisted);
+
+            Console.Write((Result)convertedToInt);
+            Console.ReadLine();
+
+        }
+        internal enum Result
+        {
+            Yes = 1,
+            No = 0,
+        }
+
+    }
+}
---
+++ TestWorkEKA/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TestWorkEKA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestWorkEKA")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("225a4eb3-7b26-4011-9c89-7d8d4cfbf785")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
